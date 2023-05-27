cb5f54a3fec67cc1357350caea2da02763e3cc73
Ayvenro <46055882+Ayvenro@users.noreply.github.com>
2023-05-27 11:44:15 +0300

    Add POST, PUT, DELETE method

--- MovieRecordsManager/Controllers/MoviesController.cs
+++ MovieRecordsManager/Controllers/MoviesController.cs
@@ -42,5 +42,65 @@ namespace MovieRecordsManager.Controllers
 			}
 			return movie;
 		}
+
+		// POST: api/Movies
+		[HttpPost]
+		public async Task<ActionResult<Movie>> PostMovie(Movie movie)
+		{
+			_dbContext.Movies.Add(movie);
+			await _dbContext.SaveChangesAsync();
+			return CreatedAtAction(nameof(GetMovie), new { id = movie.Id }, movie);
+		}
+
+		// PUT: api/Movies/3
+		[HttpPut("{id}")]
+		public async Task<IActionResult> PutMovie(int id, Movie movie)
+		{
+			if (id != movie.Id)
+			{
+				return BadRequest();
+			}
+			_dbContext.Entry(movie).State = EntityState.Modified;
+			try
+			{
+				await _dbContext.SaveChangesAsync();
+			}
+			catch (DbUpdateConcurrencyException)
+			{
+
+				if (!MovieExist(id))
+				{
+					return NotFound();
+				}
+				else
+				{
+					throw;
+				}
+			}
+			return NoContent();
+		}
+
+		// DETETE: api/Movies/3
+		[HttpDelete("{id")]
+		public async Task<IActionResult> DeleteMovie(int id)
+		{
+			if (_dbContext.Movies == null)
+			{
+				return NotFound();
+			}
+			var movie = await _dbContext.Movies.FindAsync(id);
+			if (movie == null)
+			{
+				return NotFound();
+			}
+			_dbContext.Movies.Remove(movie);
+			await _dbContext.SaveChangesAsync();
+			return NoContent();
+		}
+
+		private bool MovieExist(long id)
+		{
+			return (_dbContext.Movies?.Any(e => e.Id == id)).GetValueOrDefault();
+		}
 	}
 }
